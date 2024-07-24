908ba402eb5e747d12f90fb845015748d33acea7
Dilettante Projects <173272171+DilettanteProjects@users.noreply.github.com>
2024-07-24 19:02:20 +0200

    Preliminary code clean up

--- pymodoro.py
+++ pymodoro.py
@@ -1,28 +1,69 @@
-# V 1.05
-# Proudly written on termux in vim like a Cool Guy
-## addendum: I have regrets
+ver = 1.10
 
+
+
+#%% Init stuff(Imports, const, vars)
+# Imports
+###################################
+# Standard modules
+import pathlib
+import sys
 import os
 import datetime as dt
 import time
-import pathlib
 
+# Custom modules
+
+
+# Components
+
+
+###########imprts##################
+
+
+# Constants
+################################### 
 PATH = str(pathlib.Path(__file__).parent.resolve())
+VOLUMESTEPS = (50, 40, 30, 20, 15, 10, 5, 0)
+##############consts###############
 
-volumeSteps = (50, 40, 30, 20, 15, 10, 5, 0)
-volumeLvl = 3   # 0-7
+
+# Variables
+###################################
+
+volumeLvl = 3   # 0-7, according to VOLUMESTEPS
 
 timeSlice = dt.timedelta(minutes=25)
 timeBreak = dt.timedelta(minutes=5)
 slicesPerBlock = 4
+##############vars#################
 
 
+# Helpers
+###################################
+
+##############hlprs################
+
+
+#%%###########init#################
+
+
+#%% Classes
+###################################
+
+#%%###########classes##############
+
+
+#%% Functions
+###################################
 def play_sound(volume=volumeLvl):
-    file = f'{PATH}/neg{volumeSteps[volume]}.mp3'
+    """Play the alert sound via terminal program"""
+    file = f'{PATH}/neg{VOLUMESTEPS[volume]}.mp3'
     os.system(f'play-audio -s notification {file} &')
 
 
 def finish(delay=2.211):
+    """Special alert to mark end of block"""
     time.sleep(delay)
     play_sound()
     time.sleep(delay)
@@ -30,6 +71,7 @@ def finish(delay=2.211):
 
 
 def oneBreak(length=timeBreak):
+    """Break between slices"""
     for seconds in range(int(length.seconds)):
         os.system('clear')
         timeLeft = length - dt.timedelta(seconds=seconds)
@@ -39,6 +81,7 @@ def oneBreak(length=timeBreak):
 
 
 def oneSlice(length=timeSlice):
+    """Work-'slice'"""
     for seconds in range(int(length.seconds)):
         os.system('clear')
         timeLeft = length - dt.timedelta(seconds=seconds)
@@ -47,8 +90,8 @@ def oneSlice(length=timeSlice):
     play_sound()
     
 
-
 def block(slices=slicesPerBlock):
+    """A block of multiple slices separated by breaks"""
     for i in range(slices - 1):
         oneSlice()
         oneBreak()
@@ -56,13 +99,27 @@ def block(slices=slicesPerBlock):
     oneSlice()
     finish()
     
+    
+#%%###########fncts################
 
+
+#%% Objects
+###################################
+
+#%%###########objcts###############
+
+
+
+
+#%% Main Program
+###################################
 while True:
     os.system('clear')
     print('Time per slice:', timeSlice.seconds / 60, 'Minutes')
     print('Time per break:', timeBreak.seconds / 60, 'Minutes')
     print('Slices per block:', slicesPerBlock)
-    print('\nEnter blank for single slice, any other character for full block:')
+    print('\nEnter blank for single slice, ',
+              'any other character for full block: ', sep='')
     choice = input()
 
     os.system('termux-wake-lock')
@@ -76,3 +133,12 @@ while True:
         block()
 
     os.system('termux-wake-unlock')
+
+#%%##########main##################
+
+
+
+
+    
+
+

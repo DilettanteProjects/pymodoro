32c1b6c7cd6cedd9169d20ff70b496afbb182818
Dilettante Projects <173272171+DilettanteProjects@users.noreply.github.com>
2024-07-24 10:28:03 +0200

    V1.0 up and running
    Next up:
    Tidy up code
    Curses ui
    Android-notifications via termux
    Keep time smarter than by sleep()-ing
    Silent mode

--- pymodoro.py
+++ pymodoro.py
@@ -1,15 +1,16 @@
+# V 1.0
 # Proudly written on termux in vim like a Cool Guy
 ## addendum: I have regrets
 
 import os
-import datetime
+import datetime as dt
 import time
 
 volumeSteps = (50, 40, 30, 20, 15, 10, 5, 0)
 volumeLvl = 3   # 0-7
 
-timeSlice = datetime.timedelta(minutes=25)
-timeBreak = datetime.timedelta(minutes=5)
+timeSlice = dt.timedelta(minutes=25)
+timeBreak = dt.timedelta(minutes=5)
 slicesPerBlock = 4
 
 
@@ -19,33 +20,37 @@ def play_sound(volume=volumeLvl):
 
 
 def finish(delay=2.211):
-    play_sound()
     time.sleep(delay)
     play_sound()
     time.sleep(delay)
     play_sound()
 
-def oneSlice(lengthMinutes=timeSlice):
-    for seconds in range(int(lengthMinutes.strftime('%s'))):
+
+def oneSlice(length=timeSlice):
+    for seconds in range(int(length.seconds)):
         os.system('clear')
-        print(seconds / 60, 'Minutes done of slice')
+        timeLeft = length - dt.timedelta(seconds=seconds)
+        print(timeLeft, 'left in slice')
         time.sleep(1)
     play_sound()
-    for seconds in range(int(timeBreak.strftime('%s'))):
+    for seconds in range(int(timeBreak.seconds)):
         os.system('clear')
-        print(seconds / 60, 'Minutes done of break')
+        timeLeft = timeBreak - dt.timedelta(seconds=seconds)
+        print(timeLeft, 'left in break')
         time.sleep(1)
     play_sound()
 
-def block():
-    pass
 
-
+def block(slices=slicesPerBlock):
+    for i in range(slices):
+        oneSlice()
+    finish()
+    
 
 while True:
     os.system('clear')
-    print('Time per slice:', timeSlice.strftime('%M'), 'Minutes')
-    print('Time per break:', timeBreak.strftime('%M'), 'Minutes')
+    print('Time per slice:', timeSlice.seconds / 60, 'Minutes')
+    print('Time per break:', timeBreak.seconds / 60, 'Minutes')
     print('Slices per block:', slicesPerBlock)
     print('\nEnter blank for single slice, any other character for full block:')
     choice = input()
@@ -53,3 +58,7 @@ while True:
     # Only one slice
     if choice == '':
         oneSlice()
+    
+    # Full block
+    else:
+        block()
